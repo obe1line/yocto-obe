c08c23729dda0cdfcf81c5c3df5325a66aeee7a8
Chris Storah <chris.storah@gmail.com>
2024-01-22 17:49:33 +1100

    building with meta-imx for ov5640. Working 6.1.55 kernel. Prior to adding ap1302

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 pushd sources
-[ ! -d "poky" ] && git clone https://git.yoctoproject.org/poky --depth=1 --branch=nanbield
-[ ! -d "meta-openembedded" ] && git clone https://github.com/openembedded/meta-openembedded --depth=1 --branch=nanbield
+# [ ! -d "poky" ] && git clone https://git.yoctoproject.org/poky --depth=1 --branch=nanbield
+# [ ! -d "meta-openembedded" ] && git clone https://github.com/openembedded/meta-openembedded --depth=1 --branch=nanbield
+# [ ! -d "meta-freescale" ] && git clone https://github.com/Freescale/meta-freescale.git --depth=1 --branch=master
+# [ ! -d "meta-freescale-distro" ] && git clone https://github.com/Freescale/meta-freescale-distro.git --depth=1 --branch=master
+
+[ ! -d "poky" ] && git clone https://git.yoctoproject.org/poky --depth=1 --branch=mickledore
+[ ! -d "meta-arm" ] && git clone https://git.yoctoproject.org/meta-arm --depth=1 --branch=mickledore
+[ ! -d "meta-virtualization" ] && git clone https://git.yoctoproject.org/meta-virtualization --depth=1 --branch=mickledore
+[ ! -d "meta-openembedded" ] && git clone https://github.com/openembedded/meta-openembedded --depth=1 --branch=mickledore
 [ ! -d "meta-freescale" ] && git clone https://github.com/Freescale/meta-freescale.git --depth=1 --branch=master
 [ ! -d "meta-freescale-distro" ] && git clone https://github.com/Freescale/meta-freescale-distro.git --depth=1 --branch=master
+[ ! -d "meta-imx" ] && git clone https://github.com/nxp-imx/meta-imx.git --depth=1 --branch="rel_imx_6.1.55_2.2.0"
 popd
 
 echo Now run ". ./activate.sh" to enable the build environment
