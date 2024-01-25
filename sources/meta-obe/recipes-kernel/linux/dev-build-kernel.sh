1e5da079c537b587999a35b5f1e8212802bb07b9
Chris Storah <chris.storah@gmail.com>
2024-01-25 18:30:50 +1100

    fixed driver module patches

--- sources/meta-obe/recipes-kernel/linux/dev-build-kernel.sh
+++ sources/meta-obe/recipes-kernel/linux/dev-build-kernel.sh
@@ -2,19 +2,22 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 LINUX_VERSION="6.1.55"
-FILES_PATH=$(cd `dirname $0` && pwd -P)/linux-obe/files
+FILES_PATH=$(cd `dirname $0` && pwd -P)/linux-fslc-imx/files
 pushd ~/linux-$LINUX_VERSION
 # to add new default config settings for a later kernel, run the following commands and see .config
-# cp $FILES_PATH/obe-defconfig ~/linux-$LINUX_VERSION/.config
+# cp $FILES_PATH/obe6157-defconfig ~/linux-$LINUX_VERSION/.config
 # make olddefconfig
 #
 # apply the patches
-# git apply $FILES_PATH/0001-updated-imx8mn-dts-from-nxp-repo.patch
+# git apply $FILES_PATH/0001-new-ap1302-driver.patch
 # git apply $FILES_PATH/0002-imx8mn-ddr4-evk-Add-DTS-for-AP1302-ISP.patch
-# git apply $FILES_PATH/0003-copied-top-dtsi-for-imx8mn-from-nxp-repo.patch
-# git apply $FILES_PATH/0004-Add-camera-DTS-to-Makefile.patch
-# git apply $FILES_PATH/0005-Added-voltage-regulators-to-adv7535.patch
-# git apply $FILES_PATH/0006-Added-ap1302-driver-makefile-and-Kconfig.patch
+# git apply $FILES_PATH/0003-add-ap1302-dtb-into-makefile.patch
+# git apply $FILES_PATH/0004-added-imx708-driver.patch
+# git apply $FILES_PATH/0005-patch-media-type-header-for-imx708.patch
+# git apply $FILES_PATH/0006-added-debug-to-ap1302.patch
+# git apply $FILES_PATH/0007-added-ar0144-driver.patch
+# git apply $FILES_PATH/0008-imx8mn-ddr4-evk-Add-DTS-for-AR0144.patch
+# git apply $FILES_PATH/0009-added-ar0144-into-config.patch
 #
 # build
 # make -j`nproc`
