9430384353a93f68d4bab51f663143c59a52d3a7
Chris Storah <chris.storah@gmail.com>
2024-02-12 18:11:33 +1100

    added controls to ar0144 driver

--- sources/meta-obe/recipes-kernel/linux/dev-build-kernel.sh
+++ sources/meta-obe/recipes-kernel/linux/dev-build-kernel.sh
@@ -1,28 +1,16 @@
 # Cross compile the kernel source (outside of Yocto) for development
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
-LINUX_VERSION="6.1.55"
-FILES_PATH=$(cd `dirname $0` && pwd -P)/linux-fslc-imx/files
-pushd ~/linux-$LINUX_VERSION
-# to add new default config settings for a later kernel, run the following commands and see .config
-cp $FILES_PATH/obe6157-defconfig ~/linux-$LINUX_VERSION/.config
-make olddefconfig
-#
-# apply the patches
-# git apply $FILES_PATH/0001-new-ap1302-driver.patch
-# git apply $FILES_PATH/0002-imx8mn-ddr4-evk-Add-DTS-for-AP1302-ISP.patch
-# git apply $FILES_PATH/0003-add-ap1302-dtb-into-makefile.patch
-# git apply $FILES_PATH/0004-added-imx708-driver.patch
-# git apply $FILES_PATH/0005-patch-media-type-header-for-imx708.patch
-# git apply $FILES_PATH/0006-added-debug-to-ap1302.patch
-# git apply $FILES_PATH/0007-added-ar0144-driver.patch
-# git apply $FILES_PATH/0008-imx8mn-ddr4-evk-Add-DTS-for-AR0144.patch
-# git apply $FILES_PATH/0009-added-ar0144-into-config.patch
-# git apply $FILES_PATH/0010-added-ar0144-into-makefile.patch
-# git apply $FILES_PATH/0011-added-ISI-image-format-for-RAW12.patch
-#
+LINUX_SOURCE=$(cd ~/linux-fslc_6.6 && pwd -P)
+FILES_PATH=$(cd `dirname $0` && pwd -P)/linux-fslc/files
+pushd $LINUX_SOURCE
+
+# see dev-patch-kernel.sh for source setup
+
+# make olddefconfig
+
 # build
-# make -j`nproc`
+make -j`nproc`
 # make modules
 #
 # install modules
